44fb038b88d76224cfbb872ae0b74b4494a1b4e4
DelisG <delisemarciano@gmail.com>
2023-08-19 17:21:49 -0300

    :broom: Revisão de código

--- src/view/script.js
+++ src/view/script.js
@@ -1,13 +1,9 @@
 const cardProjetos = document.getElementById("div-projetos");
-const input_url_imagem = document.getElementById("url-imagem");
-const input_descricao = document.getElementById("descricao");
-const input_data = document.getElementById("data");
 
 async function fetchProjetos() {
-  // const response = await fetch("http://localhost:2727/projetos");
   const response = await fetch("src/models/projeto.json");
-  const projeto = await response.json();
-  return projeto;
+  const projetos = await response.json();
+  return projetos;
 }
 
 function renderProjeto(projeto) {
@@ -28,22 +24,33 @@ function renderProjeto(projeto) {
 
     const imgElement = document.createElement("img");
     imgElement.src = imagem.imagem;
-    imgElement.alt = imagem.descricao;
-    imgElement.classList.add("card-img-top");
+    imgElement.alt = imagem.titulo;
+    imgElement.classList.add("card");
 
     const overlay = document.createElement("div");
     overlay.classList.add("overlay");
 
-    const descricaoDiv = document.createElement("div");
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("project-content");
+
+    const tituloDiv = document.createElement("h2");
+    tituloDiv.classList.add("titulo");
+    tituloDiv.textContent = imagem.titulo;
+
+    const descricaoDiv = document.createElement("p");
     descricaoDiv.classList.add("descricao");
     descricaoDiv.textContent = imagem.descricao;
 
-    // const dataDiv = document.createElement("div");
-    // dataDiv.classList.add("date");
-    // dataDiv.textContent = imagem.data;
+    const tecnologiasDiv = document.createElement("p");
+    tecnologiasDiv.classList.add("tecnologias");
+    tecnologiasDiv.textContent =
+      "Tecnologias: " + imagem.tecnologias.join(", ");
 
-    overlay.appendChild(descricaoDiv);
-    // overlay.appendChild(dataDiv);
+    contentDiv.appendChild(tituloDiv);
+    contentDiv.appendChild(descricaoDiv);
+    contentDiv.appendChild(tecnologiasDiv);
+
+    overlay.appendChild(contentDiv);
 
     imageContainer.appendChild(imgElement);
     imageContainer.appendChild(overlay);
@@ -60,10 +67,10 @@ function renderProjeto(projeto) {
 
 async function init() {
   try {
-    const projeto = await fetchProjetos();
-    renderProjeto(projeto);
+    const projetos = await fetchProjetos();
+    renderProjeto(projetos);
   } catch (error) {
-    console.error("Erro ao carregar imagens:", error);
+    console.error("Erro ao carregar projetos:", error);
   }
 }
 
