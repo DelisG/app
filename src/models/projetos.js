70924131039b13048546ba69b84ef0333f96c168
DelisG <delisemarciano@gmail.com>
2023-08-22 11:14:53 -0300

    :link: Adicionando item link ao esquema

--- src/models/projetos.js
+++ src/models/projetos.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const projetoSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
-  imagem: { type: String},
-  tecnologias: [{ type: String}],
-  descricao: { type: String},
+  imagem: { type: String },
+  tecnologias: [{ type: String }],
+  descricao: { type: String },
   categoria: { type: String, required: true },
-  data: { type: Date},
-  concluido: { type: Boolean},
+  data: { type: Date },
+  concluido: { type: Boolean },
+  link: { type: String },
 });
 
 const Projeto = mongoose.model("Projeto", projetoSchema);
