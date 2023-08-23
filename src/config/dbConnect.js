4cc917478c6ee3436a5884aa5c4835de0ac241da
DelisG <delisemarciano@gmail.com>
2023-08-23 12:58:34 -0300

    :link: String de conexão com banco de dados

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://admin:delis123@cluster0.obvbuo9.mongodb.net/app"
+  "mongodb+srv://admin:delis123@cluster0.obvbuo9.mongodb.net/list"
 );
 
 let db = mongoose.connection;
